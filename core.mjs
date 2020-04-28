b12a52c860576a583b9e9bbf838e15abb5f8b0f6
Nick Rimer <nickrimer.03@gmail.com>
2020-04-28 13:58:32 +0300

    -- config out of json (try heroku fix)

--- core.mjs
+++ core.mjs
@@ -1,5 +1,68 @@
 import Discord from "discord.js";
-import config from "./config.json";
+// import config from "./config.json";
+
+const config = {
+  "guildIDs": ["614553758734614644", "351366530115698691", "701796455630766232"],
+  "updateInterval": 5000,
+  "text": {
+  "botReady": "-- channel_time_bot ready",
+    "getChannelError": "-- get channel error",
+    "clock": {
+    "zero": {
+      "00": "\uD83D\uDD5B",
+        "12": "\uD83D\uDD5B",
+        "01": "\uD83D\uDD50",
+        "13": "\uD83D\uDD50",
+        "02": "\uD83D\uDD51",
+        "14": "\uD83D\uDD51",
+        "03": "\uD83D\uDD52",
+        "15": "\uD83D\uDD52",
+        "04": "\uD83D\uDD53",
+        "16": "\uD83D\uDD53",
+        "05": "\uD83D\uDD54",
+        "17": "\uD83D\uDD54",
+        "06": "\uD83D\uDD55",
+        "18": "\uD83D\uDD55",
+        "07": "\uD83D\uDD56",
+        "19": "\uD83D\uDD56",
+        "08": "\uD83D\uDD57",
+        "20": "\uD83D\uDD57",
+        "09": "\uD83D\uDD58",
+        "21": "\uD83D\uDD58",
+        "10": "\uD83D\uDD59",
+        "22": "\uD83D\uDD59",
+        "11": "\uD83D\uDD5A",
+        "23": "\uD83D\uDD5A"
+    },
+    "half": {
+      "00": "\uD83D\uDD67",
+        "12": "\uD83D\uDD67",
+        "01": "\uD83D\uDD5C",
+        "13": "\uD83D\uDD5C",
+        "02": "\uD83D\uDD5D",
+        "14": "\uD83D\uDD5D",
+        "03": "\uD83D\uDD5E",
+        "15": "\uD83D\uDD5E",
+        "04": "\uD83D\uDD5F",
+        "16": "\uD83D\uDD5F",
+        "05": "\uD83D\uDD60",
+        "17": "\uD83D\uDD60",
+        "06": "\uD83D\uDD61",
+        "18": "\uD83D\uDD61",
+        "07": "\uD83D\uDD62",
+        "19": "\uD83D\uDD62",
+        "08": "\uD83D\uDD63",
+        "20": "\uD83D\uDD63",
+        "09": "\uD83D\uDD64",
+        "21": "\uD83D\uDD64",
+        "10": "\uD83D\uDD65",
+        "22": "\uD83D\uDD65",
+        "11": "\uD83D\uDD66",
+        "23": "\uD83D\uDD66"
+    }
+  }
+}
+}
 
 const Client = new Discord.Client();
 const { guildIDs, updateInterval, text } = config;
