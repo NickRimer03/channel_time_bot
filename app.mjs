888fa8831f3471c3914061de7cf56ea558971b7d
Nick Rimer <nickrimer.03@gmail.com>
2020-05-28 01:22:31 +0300

    -- fix .env support on prod

---
+++ app.mjs
@@ -0,0 +1,58 @@
+import Discord from "discord.js";
+import config from "./config.mjs";
+
+export default () => {
+  const Client = new Discord.Client();
+  const { guildIDs, updateInterval, text } = config;
+
+  function getClockEmoji({ h, m }) {
+    return m < 30 ? text.clock.zero[h] : text.clock.half[h];
+  }
+
+  function getTime() {
+    const date = new Date();
+    const h = `${date.getUTCHours()}`.padStart(2, "0");
+    const m = `${date.getUTCMinutes()}`.padStart(2, "0");
+    const s = `${date.getUTCSeconds()}`.padStart(2, "0");
+
+    return { h, m, s };
+  }
+
+  function timeRun(channel) {
+    if (!channel) {
+      console.log(text.getChannelError);
+      return;
+    }
+
+    Client.setInterval(() => {
+      const { h, m, s } = getTime();
+      channel.edit({ name: `${getClockEmoji({ h, m })} UTC: ${h}-${m}-${s}` }).catch((err) => console.log(err));
+    }, updateInterval);
+  }
+
+  Client.on("ready", () => {
+    console.log(text.botReady);
+
+    guildIDs.forEach((guildID) => {
+      const guild = Client.guilds.get(guildID);
+      const channel = guild.channels.array().find(({ name, type }) => type === "voice" && name.includes("UTC: "));
+
+      if (!channel) {
+        const { h, m, s } = getTime();
+        guild
+          .createChannel(`${getClockEmoji({ h, m })} UTC: ${h}-${m}-${s}`, {
+            type: "voice",
+          })
+          .then((ch) => {
+            ch.overwritePermissions(guild.roles.get(guildID), { CONNECT: false }).catch((err) => console.log(err));
+            timeRun(ch);
+          })
+          .catch((err) => console.log(err));
+      } else {
+        timeRun(channel);
+      }
+    });
+  });
+
+  Client.login(process.env.BOT_TOKEN).catch((err) => console.log(err));
+};
--- core.mjs
+++ core.mjs
@@ -1,60 +1,17 @@
-import path from "path";
-import Discord from "discord.js";
-import dotEnv from "dotenv";
-import config from "./config.mjs";
+import app from "./app.mjs";
 
-dotEnv.config({ path: path.resolve() + "/.env" });
-
-const Client = new Discord.Client();
-const { guildIDs, updateInterval, text } = config;
-
-function getClockEmoji({ h, m }) {
-  return m < 30 ? text.clock.zero[h] : text.clock.half[h];
-}
-
-function getTime() {
-  const date = new Date();
-  const h = `${date.getUTCHours()}`.padStart(2, "0");
-  const m = `${date.getUTCMinutes()}`.padStart(2, "0");
-  const s = `${date.getUTCSeconds()}`.padStart(2, "0");
-
-  return { h, m, s };
-}
-
-function timeRun(channel) {
-  if (!channel) {
-    console.log(text.getChannelError);
-    return;
-  }
-
-  Client.setInterval(() => {
-    const { h, m, s } = getTime();
-    channel.edit({ name: `${getClockEmoji({ h, m })} UTC: ${h}-${m}-${s}` }).catch(err => console.log(err));
-  }, updateInterval);
-}
-
-Client.on("ready", () => {
-  console.log(text.botReady);
-
-  guildIDs.forEach(guildID => {
-    const guild = Client.guilds.get(guildID);
-    const channel = guild.channels.array().find(({ name, type }) => type === "voice" && name.includes("UTC: "));
-
-    if (!channel) {
-      const { h, m, s } = getTime();
-      guild
-        .createChannel(`${getClockEmoji({ h, m })} UTC: ${h}-${m}-${s}`, {
-          type: "voice"
-        })
-        .then(ch => {
-          ch.overwritePermissions(guild.roles.get(guildID), { CONNECT: false }).catch(err => console.log(err));
-          timeRun(ch);
-        })
-        .catch(err => console.log(err));
-    } else {
-      timeRun(channel);
+if (process.env.BUILD_MODE !== "production") {
+  Promise.all([import("dotenv"), import("path")]).then(
+    ([
+      {
+        default: { config },
+      },
+      path,
+    ]) => {
+      config({ path: path.resolve() + "/.env" });
+      app();
     }
-  });
-});
-
-Client.login(process.env.BOT_TOKEN);
+  );
+} else {
+  app();
+}
